0fac8f2a36d4fbb8c85b2634ead60f337776582f
JamesF2440 <James.F2440@gmail.com>
2023-06-19 12:55:24 -0600

    Merged utility from .py files to methods. Added Docs

--- aEye/testing.py
+++ aEye/testing.py
@@ -9,13 +9,14 @@ if __name__ == "__main__":
     the sake of speed. Video join doesnt work currently. 
     """
     print("--- Video Processing Framework ---")
-    video = Video("inputs/testVid.mp4")
-    video.get_metadata()
+    video = Video("testVid.mp4") # or any local video, creates Video object
+    video.extract_metadata() # Pretty necessary for any video restructuring/processing
     ### Test Video Functions ###
     # All video features come as methods to the Video object, use looks like
-    # video.split_on_time(start, end, file)
+    # video.split_on_time(start, end)
     # Example:
-    video.split_interval(2)  # splits the video into 2 second clips
-    video.crop_section(400, 400, 500, 200)  # crops a 400x400 section starting at (500,200)
-    video.extract_by_frame(45)  # extracts the 45th frame
+    video.split_on_interval(2)  # splits the video into 2 second clips
+    video.crop_video_section(400, 400, 500, 200)  # crops a 400x400 section starting at (500,200)
+    video.cv_extract_specific_frame(45)  # extracts the 45th frame
+    video.cv_extract_frame_at_time(4.655)
 
