5df9d6ee1e25f0017e6455c79a709f7d8a2fe84b
JamesF2440 <114521134+JamesF2440@users.noreply.github.com>
2023-06-19 12:59:04 -0600

    Update extract_metadata_test.py

--- tests/extract_metadata_test.py
+++ tests/extract_metadata_test.py
@@ -6,7 +6,7 @@ trash metadata test class featuring a super epic hardcoded video ref
 
 
 def test_extract_metadata():
-    new_vid_obj = Video("aEye/inputs/testVid.mp4")
+    new_vid_obj = Video("aEye/testVid.mp4")
     to_check = new_vid_obj.extract_metadata(new_vid_obj.getfile())
     codec = to_check["streams"][0]["codec_name"]
     assert codec == "h264"
