fcc60f8a4e4c317e570cddfa45d4423aadca2cce
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-06 09:27:22 -0600

    fix init file

--- aEye/yolo/__init__.py
+++ aEye/yolo/__init__.py
@@ -2,4 +2,4 @@ from .yolo import Yolo
 from .training_parameter_input import training_parameter_input
 from .prediction_parameter_input import prediction_parameter_input
 from .visualize import visualize
-from .pipeline import pipelineßßß
+from .pipeline import pipeline
