2e89d97a9e985b46bc10bf04ac85962a5c8efb4d
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-06 14:01:05 -0600

    fix init

--- aEye/__init__.py
+++ aEye/__init__.py
@@ -1,5 +1,7 @@
 from aEye.video import Video
 from aEye.processor import Processor
+from aEye.auxiliary import Aux
+
 from .yolo import Yolo
 from .yolo import training_parameter_input
 from .yolo import prediction_parameter_input
