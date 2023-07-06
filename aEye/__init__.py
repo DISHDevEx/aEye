5d4da96023bb0330e2ad454ac1d8936b2d0253fc
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-06 14:01:05 -0600

    fix init

--- aEye/__init__.py
+++ aEye/__init__.py
@@ -1,7 +1,12 @@
 from aEye.video import Video
 from aEye.processor import Processor
+from .mediapipe import object_detection
+from .mediapipe import visualize
+from .yolo import yolo
 from aEye.auxiliary import Aux
 
+from .yolo import Yolo
+from .yolo import training_parameter_input
 from .yolo import Yolo
 from .yolo import training_parameter_input
 from .yolo import prediction_parameter_input
