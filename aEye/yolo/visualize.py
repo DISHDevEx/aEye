b23ae6c736b8435e0b314dc3113e232271993669
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-06 10:11:42 -0600

    fix input

--- aEye/yolo/visualize.py
+++ aEye/yolo/visualize.py
@@ -8,31 +8,28 @@ FONT_THICKNESS = 1
 TEXT_COLOR = (255, 0, 0)  # red
 
 
-def visualize(
-    image,
-    detection_result
-) -> np.ndarray:
-  """Draws bounding boxes on the input image and return it.
-  Args:
-    image: The input RGB image.
-    detection_result: The list of all "Detection" entities to be visualize.
-  Returns:
-    Image with bounding boxes.
-  """
-  for detection in detection_result:
-    # Draw bounding_box
-    x,y,w,h = detection.boxes.xywh[0]
-    start_point = x.item(), y.item()
-    end_point = x.item() + w.item(), y.item() + h.item()
-    cv2.rectangle(image, start_point, end_point, TEXT_COLOR, 3)
+def visualize(image,detection_result) -> np.ndarray:
+    """Draws bounding boxes on the input image and return it.
+    Args:
+      image: The input RGB image.
+      detection_result: The list of all "Detection" entities to be visualize.
+    Returns:
+      Image with bounding boxes.
+    """
+    for i in range(len(detection_result)):
+        # Draw bounding_box
+        x,y,w,h = detection_result[0].boxes.xywh[i]
+        start_point = int(x.item()), int(y.item())
+        end_point = int(x.item()) + int(w.item()), int(y.item()) + int(h.item())
+        cv2.rectangle(image, start_point, end_point, TEXT_COLOR, 3)
 
-    # Draw label and score
-    category_name = detection.names[detection.boxes.cls.item()]
-    probability = round(detection.boxes.conf.item(), 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (MARGIN + x.item(),
-                     MARGIN + ROW_SIZE + y.item())
-    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
+        # Draw label and score
+        category_name = detection_result[0].names[detection_result[0].boxes.cls[i].item()]
+        probability = round(detection_result[0].boxes.conf.item(), 2)
+        result_text = category_name + ' (' + str(probability) + ')'
+        text_location = (MARGIN + int(x.item()),
+                        MARGIN + ROW_SIZE + int(y.item()))
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
 
-  return image
+    return image
