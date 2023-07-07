51c3669f2b621bb05fa361248903064811cc89c3
JamesF2440 <James.F2440@gmail.com>
2023-07-07 17:25:00 -0600

    pr ready (i hope)

--- aEye/__init__.py
+++ aEye/__init__.py
@@ -1,4 +1,5 @@
 from aEye.video import Video
+<<<<<<< HEAD
 from aEye.auxiliary import Aux
 from aEye.processor import Processor
 <<<<<<< HEAD
@@ -18,3 +19,10 @@ from .mediapipe import visualize
 from .mediapipe import object_detection
 from .mediapipe import visualize
 >>>>>>> 186cdeb (pushing for Docker work)
+=======
+from .mediapipe import object_detection
+from .mediapipe import visualize
+from aEye.labeler import Labeler
+from aEye.extractor import Extractor
+from aEye.auxiliary import Aux
+>>>>>>> 2b9d943 (pr ready (i hope))
