0c814679d2db6057cea208c3153c7a9a936dd61f
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-09 09:54:10 -0600

    trigger

--- aEye/labeler.py
+++ aEye/labeler.py
@@ -2,7 +2,9 @@ import logging
 from static_ffmpeg import run
 import math
 
-ffmpeg, ffprobe = run.get_or_fetch_platform_executables_else_raise()
+#Please comment this out when setting up a docker image.
+#This will fail when we use the docker image in the lambda function on AWS.
+#ffmpeg, ffprobe = run.get_or_fetch_platform_executables_else_raise()
 
 
 class Labeler:
