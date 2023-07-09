fbea5cc6a3065d5a492c9961fb736b8e1a1d1b12
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-09 07:42:36 -0600

    trigger

--- aEye/__init__.py
+++ aEye/__init__.py
@@ -2,7 +2,7 @@ from aEye.video import Video
 from aEye.auxiliary import Aux
 from aEye.processor import Processor
 
-from .yolov8 import yolo
+from .yolov8 import Yolo
 from .yolov8 import training_parameter_input
 from .yolov8 import prediction_parameter_input
 from .yolov8 import visualize_yolo
