660f4ad12c3284f42c48c26d4f44e33ed033d21f
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-10 07:48:21 -0600

    fix import

--- aEye/__init__.py
+++ aEye/__init__.py
@@ -9,5 +9,6 @@ from .mediapipe import visualize
 from .yolo import Yolo
 from .yolo import training_parameter_input
 from .yolo import prediction_parameter_input
-
+from .yolo import visualize_yolo
+from .yolo import pipeline
 
