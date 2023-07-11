5b8ced092cfb7a02989037038134c2d4b57df227
vinayakSharm <vinayak.sharma@dish.com>
2023-07-11 19:34:47 +0000

    cleaning up reconstruction module

--- aEye/RealBasicVSR_reconstruction_module/__init__.py
+++ aEye/RealBasicVSR_reconstruction_module/__init__.py
@@ -6,4 +6,4 @@ from .builder import build_backbone
 from .builder import build_component
 from .builder import build_loss
 from .builder import build_model
-from .builder import download_model 
+from .builder import download_model
--- aEye/RealBasicVSR_reconstruction_module/builder.py
+++ aEye/RealBasicVSR_reconstruction_module/builder.py
@@ -3,18 +3,21 @@ from mmcv import build_from_cfg
 from mmedit.models.registry import BACKBONES, COMPONENTS, LOSSES, MODELS
 import boto3
 
+
 def build(cfg, registry, default_args=None):
     """Build module function.
 
-    Args:
-        cfg (dict): Configuration for building modules.
-        registry (obj): ``registry`` object.
-        default_args (dict, optional): Default arguments. Defaults to None.
+    Parameters
+    ----------
+        cfg: dict
+            Configuration for building modules.
+        registry: obj
+            ``registry`` object.
+        default_args: dict
+            Default arguments. Defaults to None.
     """
     if isinstance(cfg, list):
-        modules = [
-            build_from_cfg(cfg_, registry, default_args) for cfg_ in cfg
-        ]
+        modules = [build_from_cfg(cfg_, registry, default_args) for cfg_ in cfg]
         return nn.Sequential(*modules)
 
     return build_from_cfg(cfg, registry, default_args)
@@ -23,8 +26,10 @@ def build(cfg, registry, default_args=None):
 def build_backbone(cfg):
     """Build backbone.
 
-    Args:
-        cfg (dict): Configuration for building backbone.
+    Parameters
+    ----------
+        cfg: dict
+            Configuration for building backbone.
     """
     return build(cfg, BACKBONES)
 
@@ -32,8 +37,10 @@ def build_backbone(cfg):
 def build_component(cfg):
     """Build component.
 
-    Args:
-        cfg (dict): Configuration for building component.
+    Parameters
+    ----------
+        cfg: dict
+            Configuration for building component.
     """
     return build(cfg, COMPONENTS)
 
@@ -41,8 +48,10 @@ def build_component(cfg):
 def build_loss(cfg):
     """Build loss.
 
-    Args:
-        cfg (dict): Configuration for building loss.
+    Parameters
+    ----------
+        cfg: dict
+            Configuration for building loss.
     """
     return build(cfg, LOSSES)
 
@@ -50,18 +59,23 @@ def build_loss(cfg):
 def build_model(cfg, train_cfg=None, test_cfg=None):
     """Build model.
 
-    Args:
-        cfg (dict): Configuration for building model.
-        train_cfg (dict): Training configuration. Default: None.
-        test_cfg (dict): Testing configuration. Default: None.
+    Parameters
+    ----------
+        cfg: dict
+            Configuration for building model.
+        train_cfg: dict
+            Training configuration. Default: None.
+        test_cfg: dict
+            Testing configuration. Default: None.
     """
     return build(cfg, MODELS, dict(train_cfg=train_cfg, test_cfg=test_cfg))
+
 
 def download_model(local_path, bucket_name, key):
     """
     Downloads any model file from s3 to a local path.
-    
-     Parameters
+
+    Parameters
     ----------
 
     local_path: string
@@ -69,10 +83,9 @@ def download_model(local_path, bucket_name, key):
     bucket_name: string
         The bucket name where the files belong in the S3 bucket.
     key: string
-        The name of the object and any preceeding folders. 
+        The name of the object and any preceeding folders.
     """
-    s3 = boto3.client('s3')
-    with open(local_path, 'wb') as file:
+    s3 = boto3.client("s3")
+    with open(local_path, "wb") as file:
         s3.download_fileobj(bucket_name, key, file)
     return print(f"Downloaded file to: {local_path}")
-    
