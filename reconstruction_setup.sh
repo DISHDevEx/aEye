889d560a94c7b6850f7e8ee0a9aa2c33bffcbfcd
vinayakSharm <vinayak.sharma@dish.com>
2023-07-14 20:21:48 +0000

    modifying setup sh

--- reconstruction_setup.sh
+++ reconstruction_setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
 activate base
-
+pip install torch torchvision torchaudio 
+pip install openmim
+mim install mmcv-full
+pip install mmedit
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
-conda update -n base -c defaults conda -y
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch -y 
-
-python3 -m pip install openmim
-python3 -m mim install mmcv-full
-python3 -m pip install mmedit
