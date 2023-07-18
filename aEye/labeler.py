33cf05c2210f4e755f0c9f275d705b089bc6efe0
JamesF2440 <James.F2440@gmail.com>
2023-07-18 14:41:52 -0600

    updated for pr

--- aEye/labeler.py
+++ aEye/labeler.py
@@ -114,12 +114,10 @@ class Labeler:
         # Go to each video and add the resizing ffmpeg label.
         for video in video_list:
             try:
-                # assert x_ratio in range(0,1) and y_ratio in range(0,1)
+                assert x_ratio in range(0,1) and y_ratio in range(0,1)
                 video.extract_metadata()
                 new_width = int(video.get_width() * x_ratio)
                 new_height = int(video.get_height() * y_ratio)
-
-                # video.add_label(f"-vf scale={math.ceil(new_width / 2) * 2}:{math.ceil(new_height / 2) * 2},setsar=1:1 ")
                 video.complex_filter.append(
                     f"scale={math.ceil(new_width / 2) * 2}:{math.ceil(new_height / 2) * 2},setsar=1:1")
                 video.add_output_title(f"resized_ratio_{x_ratio}_{y_ratio}_")
