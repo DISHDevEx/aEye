869919ccf79c63eae4e06e64161416742b83dd97
JamesF2440 <James.F2440@gmail.com>
2023-07-19 13:16:13 -0600

    should work

--- aEye/video.py
+++ aEye/video.py
@@ -143,10 +143,10 @@ class Video:
                 The dictionary of metadata for all streams.
 
         """
-        # paths1 = subprocess.check_output("static_ffmpeg_paths", shell=True).decode('utf-8')
+        #paths1 = subprocess.check_output("static_ffmpeg_paths", shell=True).decode('utf-8')
         # paths = paths1.split('\n')[1]
         # probe_path = paths.split('=')[0]
-        probe_path = run.get_or_fetch_platform_executables_else_raise()
+        ffmpeg, probe_path = run.get_or_fetch_platform_executables_else_raise()
         if self.meta_data is None:
             fp = None
             if self.file is None:
