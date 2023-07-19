6bbf5b4ed2fe57772351e44e7dcd75b797a269b4
JamesF2440 <James.F2440@gmail.com>
2023-07-19 14:59:01 -0600

    fix mistake

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,7 @@ def handler(event, context):
     print("Videos and Util loaded")
     label.change_resolution(video_list, '720p')
     label.change_fps(video_list, 10)
-    out = aux.execute_label_and_write_local(video_list[1], '/tmp/')
+    out = aux.execute_label_and_write_local(video_list, '/tmp/')
     print("Videos executed")
     aux.upload_s3(out, bucket='aeye-data-bucket', prefix='modified/')
     retval = "Videos Uploaded!"
