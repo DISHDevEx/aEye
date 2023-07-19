49089c17cf04e082f1330619ab81205b0e5d098a
JamesF2440 <James.F2440@gmail.com>
2023-07-19 15:11:43 -0600

    wait for file generation

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ from aEye.video import Video
 from aEye.auxiliary import Aux
 from aEye.labeler import Labeler
 from aEye.extractor import Extractor
+import time
 
 def handler(event, context):
     print("Running aEye lambda test!")
@@ -16,6 +17,7 @@ def handler(event, context):
     label.change_fps(video_list, 10)
     out = aux.execute_label_and_write_local(video_list, '/tmp')
     print("Videos executed")
+    time.sleep(5)
     aux.upload_s3(out, bucket='aeye-data-bucket', prefix='modified/')
     retval = "Videos Uploaded!"
     return retval
